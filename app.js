bf469491c9bd8f496fb3eaaa2f685647993fabe0
Jeevitha <jeevithac1999@gmail.com>
2020-04-03 12:20:01 +0530

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var helmet = require('helmet');
 var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://jeevitha:f8pnNlsM2Lest4P5@cluster0-zs321.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://jeevitha:f8pnNlsM2Lest4P5@cluster0-zs321.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
